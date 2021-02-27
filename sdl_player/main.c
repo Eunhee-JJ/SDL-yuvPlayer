29a2697bed1e7392770897c69de165a942dd03f4
Eunhee-JJ <eunhee0324@g.skku.edu>
2021-02-27 17:21:09 +0900

    개발환경 세팅 완료

---
+++ sdl_player/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sdl_player
+//
+//  Created by 정은희 on 2021/02/27.
+//
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
