3a63f7ba5506685b961bc5b181162628553f27e6
Eunhee-JJ <eunhee0324@g.skku.edu>
2021-03-01 21:16:41 +0900

    [FIX] ffmpeg 프로젝트에서 제거

--- sdl_player/main.c
+++ sdl_player/main.c
@@ -1,41 +1,136 @@
-//
-//  main.c
-//  sdl_player
-//
-//  Created by 정은희 on 2021/02/27.
-//
-
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_video.h>
+#include <stdbool.h>
+ 
+// Display window size
+int WINDOW_W = 1280;
+int WINDOW_H = 720;
+ 
+int thread_exit = 0;
 
-int main(int argc, const char * argv[]) {
-    static int display_in_use = 0; /* Only using first display */
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Texture *texture;
 
-    int i, display_mode_count;
-    SDL_DisplayMode mode;
-    Uint32 f;
+FILE *fp;
+
+int refresh_video(void *opaque)
+{
+    while(thread_exit == 0)
+    {
+        SDL_Event event;
+        event.type = SDL_USEREVENT + 1;
+        SDL_PushEvent(&event);
+        SDL_Delay(40);
+    }
+    printf("exit refresh video thread.\n");
+    return 0;
+}
+
+bool init()
+{
+    bool success = true;
     
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Log("SDL_GetNumVideoDisplays(): %i", SDL_GetNumVideoDisplays());
-
-    display_mode_count = SDL_GetNumDisplayModes(display_in_use);
-    if (display_mode_count < 1) {
-        SDL_Log("SDL_GetNumDisplayModes failed: %s", SDL_GetError());
-        return 1;
+    if( SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
     }
-    SDL_Log("SDL_GetNumDisplayModes: %i", display_mode_count);
+    else{
+        window = SDL_CreateWindow("SDL player", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_W, WINDOW_H, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+     
+        renderer = SDL_CreateRenderer(window, -1, 0);
+     
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, WINDOW_W, WINDOW_H);
+    }
+    
+    return success;
+}
 
-    for (i = 0; i < display_mode_count; ++i) {
-        if (SDL_GetDisplayMode(display_in_use, i, &mode) != 0) {
-            SDL_Log("SDL_GetDisplayMode failed: %s", SDL_GetError());
-            return 1;
+bool loadMedia()
+{
+    bool success = true;
+    
+    fp = fopen("PeopleOnStreet_1280x720_30_Original.yuv", "rb");
+    if (!fp)
+    {
+        printf("Cannot open the file.\n");
+        success = false;
+    }
+    
+    return success;
+}
+
+void SDLclose()
+{
+    //Destory window
+    SDL_DestroyWindow(window);
+    window = NULL;
+    
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+ 
+int main(int argc, char* args[])
+{
+    if( !init() )
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        //Load Media
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
         }
-        f = mode.format;
-
-        SDL_Log("Mode %i\tbpp %i\t%s\t%i x %i", i,
-        SDL_BITSPERPIXEL(f), SDL_GetPixelFormatName(f), mode.w, mode.h);
+        else{
+            //Main loop flag
+            bool quit = false;
+            bool play = true;
+            
+            SDL_Thread *thread = SDL_CreateThread(refresh_video, NULL, NULL);
+            SDL_Event event;
+            SDL_Rect rect;
+            
+            // The width and height of the image
+            const int IMG_W = 1280;
+            const int IMG_H = 720;
+            
+            // buffer size for YUV420
+            uint8_t buff[IMG_W * IMG_H * 3 / 2];
+            
+            while( !quit )
+            {
+                while( SDL_PollEvent( &event ) != 0 )
+                {
+                    if( event.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                    else if(event.type == SDL_WINDOWEVENT)
+                    {
+                        SDL_GetWindowSize(window, &WINDOW_W, &WINDOW_H);
+                        printf("window resize, w=%d h=%d\n", WINDOW_W, WINDOW_H);
+                    }
+                }
+                
+                // loop for reading frames & update render
+                fread(buff, 1, IMG_W * IMG_H * 3 / 2, fp);
+                SDL_UpdateTexture(texture, NULL, buff, IMG_W);
+     
+                rect.x = 0;
+                rect.y = 0;
+                rect.w = WINDOW_W;
+                rect.h = WINDOW_H;
+     
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, texture, NULL, &rect);
+                SDL_RenderPresent(renderer);
+                
+                SDL_Delay(40);
+            }
+        }
     }
+    SDLclose();
     return 0;
 }
