80e76b5555de894c21aaa6c12df6716f65df11a3
Eunhee-JJ <eunhee0324@g.skku.edu>
2021-03-03 21:40:58 +0900

    [FEATURE] 프레임 당 소요시간 산정 방법 변경
    기존 방법: 총 시간/읽은 프레임 수 -> 새 방법: fread 시점부터 화면에 뿌려지는 시간을 기록

--- sdl_player/main.c
+++ sdl_player/main.c
@@ -14,9 +14,20 @@ SDL_Texture *texture;
 
 FILE *fp;
 
-unsigned int startTime = 0, endTime, restartTime = 0;
-int fps;
+unsigned int startTime, endTime, FTime;
 
+// The width and height of the image
+const int IMG_W = 1280;
+const int IMG_H = 720;
+const int IMG_F = 30;
+int nowFrame = 0;
+int nFrame = 0;
+
+// buffer size for YUV420
+uint8_t buff[IMG_W * IMG_H * 3 / 2];
+
+bool play = true;
+/*
 int refresh_video(void *opaque)
 {
     while(thread_exit == 0)
@@ -29,7 +40,7 @@ int refresh_video(void *opaque)
     printf("exit refresh video thread.\n");
     return 0;
 }
-
+*/
 bool init()
 {
     bool success = true;
@@ -63,6 +74,37 @@ bool loadMedia()
     return success;
 }
 
+void displayFrame(SDL_Rect *rect)
+{
+    // loop for reading frames & update render
+    startTime = SDL_GetTicks();
+    fread(buff, 1, IMG_W * IMG_H * 3 / 2, fp);
+    nowFrame++;
+    nFrame++;
+    //printf( "Now frame: %d\n", nowFrame );
+    SDL_UpdateTexture(texture, NULL, buff, IMG_W);
+
+    rect->x = 0;
+    rect->y = 0;
+    rect->w = WINDOW_W;
+    rect->h = WINDOW_H;
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, rect);
+    SDL_RenderPresent(renderer);
+    
+    endTime = SDL_GetTicks();
+    FTime = endTime - startTime;
+}
+
+void printResult()
+{
+    printf( "재생 완료. \n" );
+    printf( "프레임 당 소요시간 : %dms\n", FTime);
+    printf( "총 소요시간: %dms\n", FTime * nFrame );
+    printf( "play: %d\n", play );
+}
+
 void SDLclose()
 {
     //Destory window
@@ -73,7 +115,6 @@ void SDLclose()
     SDL_Quit();
 }
 
- 
 int main(int argc, char* args[])
 {
     if( !init() )
@@ -91,76 +132,49 @@ int main(int argc, char* args[])
             // Main loop flag
             bool quit = false;
             // Controlling video play flag
-            bool play = true;
             
-            SDL_Thread *thread = SDL_CreateThread(refresh_video, NULL, NULL);
+            //SDL_Thread *thread = SDL_CreateThread(refresh_video, NULL, NULL);
             SDL_Event event;
             SDL_Rect rect;
             
-            // The width and height of the image
-            const int IMG_W = 1280;
-            const int IMG_H = 720;
-            const int IMG_F = 30;
-            int nowFrame = 0;
-            int nFrame = 0;
-            
-            // buffer size for YUV420
-            uint8_t buff[IMG_W * IMG_H * 3 / 2];
             memset(buff, 0, IMG_W * IMG_H * 3 / 2);
             
             // Main loop
             while( !quit )
             {
-                startTime = 0;
                 while( SDL_PollEvent( &event ) != 0 )
                 {
                     if( event.type == SDL_QUIT )
                     {
                         quit = true;
                     }
-                    else if( event.type == SDL_WINDOWEVENT )
-                    {
-                        SDL_GetWindowSize(window, &WINDOW_W, &WINDOW_H);
-                        printf("window resize, w=%d h=%d\n", WINDOW_W, WINDOW_H);
-                    }
-                    else if( event.key.keysym.sym ==  SDLK_SPACE ){
+                    else if( event.key.keysym.sym == SDLK_SPACE ){
                         play = !play;
                         if( play == true ){
                             if( nowFrame == IMG_F ){
-                                rewind( fp );
                                 printf(" rewind \n");
+                                rewind( fp );
                                 nowFrame = 0;
                             }
-                            restartTime = SDL_GetTicks();
                             nFrame = 0;
                         }
                     }
-                    else if( play == true && nowFrame < IMG_F ){
-                        // loop for reading frames & update render
-                        fread(buff, 1, IMG_W * IMG_H * 3 / 2, fp);
-                        nowFrame++;
-                        nFrame++;
-                        //printf( "Now frame: %d\n", nowFrame );
-                        SDL_UpdateTexture(texture, NULL, buff, IMG_W);
-             
-                        rect.x = 0;
-                        rect.y = 0;
-                        rect.w = WINDOW_W;
-                        rect.h = WINDOW_H;
-             
-                        SDL_RenderClear(renderer);
-                        SDL_RenderCopy(renderer, texture, NULL, &rect);
-                        SDL_RenderPresent(renderer);
+                    else if( event.key.keysym.sym == SDLK_LEFT ){
+                        //fseek()
+                        printf("Previous Frame\n");
+                        nowFrame--;
+                        nFrame = 1;
+                        fseek(fp, IMG_W * IMG_H * 3 / 2 ,SEEK_CUR);
+                        displayFrame(&rect);
+                        printResult();
+                    }
+                    if( play == true && nowFrame < IMG_F ){
+                        displayFrame(&rect);
                         
-                        //SDL_Delay(40);
+                        // Stop playing at the end of file
                         if(nowFrame == IMG_F){
-                            endTime = SDL_GetTicks();
-                            fps = (endTime - restartTime) / nFrame;
-                            printf( "재생 완료. \n" );
-                            printf( "프레임 당 소요시간 : %dms\n", fps);
-                            printf( "총 소요시간: %dms\n", endTime - restartTime );
                             play = !play;
-                            printf( "play: %d", play );
+                            printResult();
                         }
                     }
                 }
